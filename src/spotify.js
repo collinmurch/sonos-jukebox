33a3ba51b3034ffa8edb44156f9089b968504ed1
Collin Murch <collin@murch.net>
2018-07-02 19:22:35 -0700

    Fixed issue.
    .

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const querystring = require('querystring');
 
-const token = 'BQAfXbzQMSsRf0Tf0xfPvtmns_AhWzWSnrgz1OGu78Mqxh9-1ADH37BAdET3754Hn2KEhsMC8vXA35Q3SDkzk0W_HviDaOY-NQfp7T_n69op991aY51ig2YBd03-FsN8hrQZhoCZGPBWRsV9hbc9GvJhR72tgQKJE9o';
+const token = process.env.SPOTIFY_TOKEN;
 
 var tracks = (query) => {
     let results = [];
